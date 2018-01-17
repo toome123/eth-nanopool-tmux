00b5931a165194e37383e41c3baddb78afc08ba0
Dimitar Manovski <toome123@hotmail.co.uk>
2018-01-17 13:07:04 +0200

    remove cuda

--- nanopool-mon.py
+++ nanopool-mon.py
@@ -4,7 +4,7 @@ import requests
 import collections
 import sys
 
-url = "http://nanopool.org/api/user/{}".format(sys.argv[1])
+url = "https://api.nanopool.org/v1/eth/user/{}".format(sys.argv[1])
 req = requests.get(url)
 
 data = req.json()["data"]
@@ -12,15 +12,3 @@ data = req.json()["data"]
 print("Account: " + data["account"])
 print("Balance: " + data["balance"])
 print("Current hashrate: " + data["hashrate"])
-
-for worker in data["workers"]:
-
-    worker = collections.OrderedDict(sorted(worker.items()))
-    print("\nWorker: " + worker["id"])
-    print("Individual hashrate: " + worker["hashrate"])
-    for k, v in worker.items():
-        if (k != "id") and (k != "hashrate"):
-            k = k.split("_")
-            k[0] = k[0].replace("avg", "Average")
-            k[1] = k[1].upper()
-            print(k[0] + " " + k[1] + ": " + v)
